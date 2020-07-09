936517dbea367a4b80c3c62a5e9b96122ff8395a
Siarhei Dudko <slavianich@gmail.com>
2020-07-09 23:42:48 +0300

    fix: site

--- hosting.sh
+++ hosting.sh
@@ -3,8 +3,8 @@ MYHOSTDIRNAME="docs"
 rm -rf $MYHOSTDIRNAME
 typedoc src
 rsync -ra hosting/ $MYHOSTDIRNAME
-echo "firebase-engine.ml\n">$MYHOSTDIRNAME/CNAME
-echo "\n">$MYHOSTDIRNAME/.nojekyll
+echo "firebase-engine.ml">$MYHOSTDIRNAME/CNAME
+echo "">$MYHOSTDIRNAME/.nojekyll
 OLDTEXT="<meta name=\"description\" content=\"\">"
 NEWTEXT=$(cat "hosting.html" | tr "\n" "\0")
 find $MYHOSTDIRNAME -type f -name "*.html" -exec sed -i -e "s%$OLDTEXT%$NEWTEXT%g" {} +
