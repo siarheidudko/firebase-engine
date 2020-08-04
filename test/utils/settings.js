6b909b91f7e9129eb05eb7c235664410679343fa
Siarhei Dudko <slavianich@gmail.com>
2020-08-05 00:45:47 +0300

    test

--- test/utils/settings.js
+++ test/utils/settings.js
@@ -3,4 +3,3 @@ const path = require("path")
  * path to firebase service account
  */
 module.exports.serviceAccountPath = path.join(__dirname, "../../serviceAccount.json")
-//module.exports.serviceAccountPath = "/Users/siarhei/Downloads/test-ea2c8-firebase-adminsdk-hlq5e-3a74c5485c.json"
